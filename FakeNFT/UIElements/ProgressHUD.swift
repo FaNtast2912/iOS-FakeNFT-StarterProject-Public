2ef247782150ef079a032ae9f8d00de57dd76a9d
Kaider <kronoz@yandex.ru>
2025-05-25 20:15:47 +0300

    Добавил progressHUD

--- FakeNFT/UIElements/ProgressHUD.swift
+++ FakeNFT/UIElements/ProgressHUD.swift
@@ -1,18 +1,60 @@
+
 //
 //  ProgressHUD.swift
 //  FakeNFT
 //
-//  Created by Kaider on 25.05.2025.
+//  Created by Kaider on 24.05.2025.
 //
 
 import SwiftUI
 
 struct ProgressHUD: View {
+    let isLoading: Bool
+
+    init(isLoading: Bool) {
+        self.isLoading = isLoading
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isLoading {
+            ZStack {
+
+                Color(uiColor: .systemBackground)
+                    .opacity(0.7)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {}
+
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .scaleEffect(1.5)
+                    .frame(width: 82, height: 82)
+                    .background(Color(uiColor: .secondarySystemBackground))
+                    .cornerRadius(16)
+            }
+            .transition(.opacity)
+        }
     }
 }
 
-#Preview {
-    ProgressHUD()
+
+// MARK: - Extention для использования в проекте
+// .progressHUD(isLoading:)
+
+extension View {
+    func progressHUD(isLoading: Bool) -> some View {
+        self.overlay(
+            ProgressHUD(isLoading: isLoading)
+        )
+    }
+}
+
+
+struct ProgressHUD_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.gray.opacity(0.2)
+            Text("Фоновый контент")
+        }
+        .progressHUD(isLoading: true)
+    }
 }
