20a9f1a12755a3a8dcc4f2004dc1c37b82662c6b
Kaider <kronoz@yandex.ru>
2025-05-25 20:14:10 +0300

    Добавил progressHUD

---
+++ FakeNFT/UIElements/ProgressHUD.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressHUD.swift
+//  FakeNFT
+//
+//  Created by Kaider on 25.05.2025.
+//
+
+import SwiftUI
+
+struct ProgressHUD: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressHUD()
+}
