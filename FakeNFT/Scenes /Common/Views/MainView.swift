f1643fcf94f5fdb5eb43486d63e3524b01159820
Olga Trofimova <k12to1@yandex.ru>
2025-05-25 23:58:01 +0300

    [Feature] Add TabView with NavigationModel

--- FakeNFT/Scenes /Common/Views/MainView.swift
+++ FakeNFT/Scenes /Common/Views/MainView.swift
@@ -8,20 +8,69 @@
 import SwiftUI
 
 struct AppTabView: View {
+    @StateObject private var navigationModel = NavigationModel()
     
     init() {
         // по идее надо перенести куда-то во вью модель или оставить здесь
         let servicesAssembly = ServicesAssembly(
-              networkClient: DefaultNetworkClient(),
-              nftStorage: NftStorageImpl()
-          )
+            networkClient: DefaultNetworkClient(),
+            nftStorage: NftStorageImpl()
+        )
+        
+        // настраиваем иконки таб бара
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor(Color.ypBlack)]
+        tabBarAppearance.stackedLayoutAppearance.normal.iconColor = UIColor.black
+        tabBarAppearance.stackedLayoutAppearance.selected.iconColor = UIColor(Color.ypBlueUniversal)
+        UITabBar.appearance().standardAppearance = tabBarAppearance
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $navigationModel.path) {
+            TabView(selection: $navigationModel.selectedTab) {
+                ProfileView()
+                    .tabItem {
+                        Text("Профиль")
+                        Image("yp.profileIcon")
+                            .renderingMode(.template)
+                    }
+                    .tag(0)
+                
+                CatalogView()
+                    .tabItem {
+                        Text("Каталог")
+                        Image("yp.catalogIcon")
+                            .renderingMode(.template)
+                    }
+                    .tag(1)
+                
+                CartView()
+                    .tabItem {
+                        Text("Корзина")
+                        Image("yp.cartIcon")
+                            .renderingMode(.template)
+                    }
+                    .tag(2)
+                
+                StatisticsView()
+                    .tabItem {
+                        Text("Статистика")
+                        Image("yp.statisticsIcon")
+                            .renderingMode(.template)
+                    }
+                    .tag(3)
+            }
+            // Общая обработка навигации для всех табов
+            .navigationDestination(for: Screens.self) { screen in
+                
+            }
+        }
+        .environmentObject(navigationModel)
     }
 }
 
 #Preview {
     AppTabView()
+        .environmentObject(NavigationModel())
 }
