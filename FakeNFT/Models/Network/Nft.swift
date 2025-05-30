2d4392cf6a4c36e53768e49d0a1d5ff152128991
FaNtast2912 <z_maks@inbox.ru>
2025-05-30 11:34:10 +0300

    поправили модель

--- FakeNFT/Models/Network/Nft.swift
+++ FakeNFT/Models/Network/Nft.swift
@@ -5,7 +5,7 @@ struct Nft: Codable {
     let name: String
     let createdAt: String
     let images: [URL]
-    let raiting: Int
+    let rating: Int
     let description: String
     let price: Float
     let author: String
