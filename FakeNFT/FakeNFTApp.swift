a391eaea3794349ac08f9a08e1d6d9515246786f
Kaider <kronoz@yandex.ru>
2025-06-01 12:34:37 +0300

    Удалены файлы и изменения из эпика Cart (случайно попали в develop)

--- FakeNFT/FakeNFTApp.swift
+++ FakeNFT/FakeNFTApp.swift
@@ -3,18 +3,12 @@ import SwiftUI
 @main
 struct FakeNFTApp: App {
     @StateObject private var navigation = NavigationModel()
-    @StateObject private var mockData = MockData()
-    @StateObject private var paymentViewModel = PaymentMethodViewModel()
-
+    
     var body: some Scene {
         WindowGroup {
             AppTabView()
                 .environmentObject(navigation)
-                .environmentObject(mockData)
-                .environmentObject(paymentViewModel)
-                .onAppear {
-                    paymentViewModel.loadCurrencies(from: mockData)
-                }
         }
     }
 }
+
