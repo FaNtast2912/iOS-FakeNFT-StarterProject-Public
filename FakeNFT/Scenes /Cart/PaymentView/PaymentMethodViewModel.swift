a5eff66998ccd8f2fe7a04b5aaf906e4451d2e0c
Kaider <kronoz@yandex.ru>
2025-06-04 01:08:58 +0300

    Реализован алерт с неуспешной оплатой. Добавлено снятие выделения валюты Эмулирована успешная и неуспешная оплата через выбор валюты

--- FakeNFT/Scenes /Cart/PaymentView/PaymentMethodViewModel.swift
+++ FakeNFT/Scenes /Cart/PaymentView/PaymentMethodViewModel.swift
@@ -5,14 +5,15 @@
 //  Created by [Your Name] on [Date].
 //
 
-import Foundation
 import Combine
+import SwiftUI
 
 final class PaymentMethodViewModel: ObservableObject {
     // MARK: - Published Properties
     
     @Published private(set) var currencies: [CurrencyModel] = []
     @Published var selectedCurrency: CurrencyModel? = nil
+    @Published var showPaymentError = false
     
     // MARK: - Initialization
     
@@ -25,11 +26,25 @@ final class PaymentMethodViewModel: ObservableObject {
     }
     
     // MARK: - Private Methods
+    
     private func loadMockCurrencies() -> [CurrencyModel] {
         return [
             CurrencyModel(id: "1", name: "Bitcoin", title: "BTC", image: "yp.cripto.bitcoin"),
             CurrencyModel(id: "2", name: "Ethereum", title: "ETH", image: "yp.cripto.ethereum"),
             CurrencyModel(id: "3", name: "Dogecoin", title: "DOGE", image: "yp.cripto.dogecoin")
         ]
+    }
+    
+    @MainActor
+    func processPayment() async -> Bool {
+        showPaymentError = false
+        // Если валюта не выбрана - будет ошибка оплаты
+        guard selectedCurrency != nil else {
+            try? await Task.sleep(nanoseconds: 50_000_000)
+            showPaymentError = true
+            return false
+        }
+        // Остальные параметры
+        return true
     }
 }
